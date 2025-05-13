78d29a5b9f2b3daf5b563779e07707c82922622e
Vina del Mar <vina@duoc.acad>
2025-05-12 21:43:43 -0400

    primera subida de mi proyecto

---
+++ src/main/java/cl/duoc/msvc_envio/MsvcEnvioApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.msvc_envio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcEnvioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcEnvioApplication.class, args);
+	}
+
+}
